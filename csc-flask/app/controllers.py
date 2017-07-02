cf0890aaaac07a6df6f2dbeaa986ca665349bba8
Rohan Weeden <reweeden@alaska.edu>
2017-07-02 14:10:25 -0800

    Added support for navbar extensions such as input groups in the navbar

--- csc-flask/app/controllers.py
+++ csc-flask/app/controllers.py
@@ -10,7 +10,7 @@ def index():
     mods = []
     for mod in app.iter_blueprints():
         mods.append({
-            "nav_entry": mod.get_navbar_entry()
+            "nav_entry": mod.get_navbar_entry() if hasattr(mod, "get_navbar_entry") else "",
+            "nav_extension" : mod.get_navbar_extension() if hasattr(mod, "get_navbar_extension") else ""
         })
-        print("Adding: {}".format(mod.get_navbar_entry()))
     return render_template("index.html", modules=mods)
