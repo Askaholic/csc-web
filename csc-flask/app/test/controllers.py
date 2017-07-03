0f13507e29ed295ddb1d4a7ec0a7526082a07e31
Rohan Weeden <reweeden@alaska.edu>
2017-07-02 22:24:04 -0800

    Added custom navbar color scheme. Fixed sticky footer. Added favicon

--- csc-flask/app/test/controllers.py
+++ csc-flask/app/test/controllers.py
@@ -9,15 +9,15 @@ from flask import Blueprint, url_for
 class TestMod(Blueprint):
     def get_navbar_extension(self):
         return '''<form class="navbar-form navbar-right">
-  <div class="input-group">
-    <input type="text" class="form-control" placeholder="Search">
-    <div class="input-group-btn">
-      <button class="btn btn-default" type="submit">
-        <i class="glyphicon glyphicon-search"></i>
-      </button>
-    </div>
-  </div>
-</form>'''.format(url_for("test.index"))
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                        <div class="input-group-btn">
+                            <button class="btn btn-default" type="submit">
+                                <i class="glyphicon glyphicon-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </form>'''.format(url_for("test.index"))
 
 mod = TestMod('test', __name__, url_prefix='/test')
 
