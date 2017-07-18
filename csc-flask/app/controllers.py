06b6a76e4e60efc9f3067077031a66e2b5e11aa5
Rohan Weeden <askaholic907@gmail.com>
2017-07-18 09:28:42 -0800

    Added contact page

--- csc-flask/app/controllers.py
+++ csc-flask/app/controllers.py
@@ -5,10 +5,16 @@
 from . import app
 from flask import render_template
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
+@app.route("/contact")
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 #
 # @app.route('/ldap')
 # @ldap.login_required
